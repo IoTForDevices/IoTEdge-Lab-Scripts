0e3d776fab32a1e990a5ba14ce5f74e26949a473
mstruys <mstruys@users.noreply.github.com>
2018-07-18 13:57:29 +0000

    Refresh PATH variable + Removed setting developer mode.
    refreshevn doesn't work immediately after installing Chocolatey. Workaround to emulate refreshevn behavior. See https://stackoverflow.com/questions/46758437/how-to-refresh-the-environment-of-a-powershell-session-after-a-chocolatey-instal.

--- IoTEdgeDevelop/InstallEdgeDevTools.ps1
+++ IoTEdgeDevelop/InstallEdgeDevTools.ps1
@@ -1,26 +1,9 @@
 # PowerShell script to install Visual Studio Code, .NET Core 2.1 and Docker CE for Windows
 
-#Import-Module BitsTransfer
- 
-#$destinationFolder="c:\IoTEdgeReqs"
-#if (-Not (Test-Path -Path $destinationFolder))
-#{
-#    New-Item -ItemType directory -Path $destinationFolder
-#}
-
-#$GitInstallExe=$destinationFolder+"\git.exe"
-#$DockerInstallExe=$destinationFolder+"\dockerwin.exe"
-#$VSCodeInstallExe=$destinationFolder+"\vscode.exe"
-#$NetCoreInstallExe=$destinationFolder+"\netcore.exe"
-
-#$GitUri="http://git-scm.com/download/win"
-#$DockerUri="https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe"
-#$VSCodeUri="https://go.microsoft.com/fwlink/?Linkid=852157"
-#$NetCoreUri="https://download.microsoft.com/download/4/0/9/40920432-3302-47a8-b13c-bbc4848ad114/dotnet-sdk-2.1.302-win-x64.exe"
-
 #Install Git, docker, visual studio code and dot net core 2.0 through the Chocolatey installer
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
+
 choco install git
 choco install docker-for-windows
 choco install dotnetcore-sdk 
@@ -28,21 +11,16 @@ choco install vscode
 choco install vscode-csharp
 choco install vscode-docker
 
+# Make `refreshenv` available right away, by defining the $env:ChocolateyInstall variable
+# and importing the Chocolatey profile module.
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
-# Download pre-requisites for Azure IoT Edge Module Development
-#Start-BitsTransfer -Source $GitUri -Destination $GitInstallExe
-#Start-BitsTransfer -Source $VSCodeUri -Destination $VSCodeInstallExe
-#Start-BitsTransfer -Source $NetCoreUri -Destination $NetCoreInstallExe
-#Start-BitsTransfer -Source $DockerUri -Destination $DockerInstallExe
+# refreshenv is now an alias for Update-SessionEnvironment
+# (rather than invoking refreshenv.cmd, the *batch file* for use with cmd.exe)
+# This should make code (Visual Studio Code) accessible via the refreshed $env:PATH, so that it can be 
+# called by name only.
+refreshenv
 
-# Install the tools
-#& $GitInstallExe /SP /VerySilent | Out-Null
-#& $VSCodeInstallExe /SP /VERYSILENT | Out-Null
-#& $DockerInstallExe | Out-Null
-#& $NetCoreInstallExe /quiet | Out-Null
-
-# Add Visual Studio Code extensions
 code --install-extension vsciot-vscode.azure-iot-edge
-#code --install-extension ms-vscode.csharp
-#code --install-extension peterjausovec.vscode-docker
 
