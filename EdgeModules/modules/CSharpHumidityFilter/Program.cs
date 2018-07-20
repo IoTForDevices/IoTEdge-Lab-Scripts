c69db337db4e48153098313372286edff24a6973
mstruys <mstruys@users.noreply.github.com>
2018-07-20 14:26:32 +0000

    Added a second Edge Module

--- EdgeModules/modules/CSharpHumidityFilter/Program.cs
+++ EdgeModules/modules/CSharpHumidityFilter/Program.cs
@@ -1,7 +1,7 @@
-namespace CSharpFilterModule
+namespace CSharpHumidityFilter
 {
     using System;
-    using System.Collections.Generic;   // For KeyValuePair<>
+    using System.Collections.Generic;
     using System.IO;
     using System.Runtime.InteropServices;
     using System.Runtime.Loader;
@@ -11,13 +11,13 @@ namespace CSharpFilterModule
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
-    using Microsoft.Azure.Devices.Shared;   // For TwinCollection
-    using Newtonsoft.Json;                  // For JsonConvert
+    using Microsoft.Azure.Devices.Shared;
+    using Newtonsoft.Json;
 
     class Program
     {
         static int counter;
-        static int temperatureThreshold { get; set; } = 103;
+        static int humidityFilter { get; set; } = 25;
 
         class MessageBody
         {
@@ -73,29 +73,29 @@ namespace CSharpFilterModule
             await ioTHubModuleClient.OpenAsync();
             Console.WriteLine("IoT Hub module client initialized.");
 
-            // Read the TemperatureThreshold value from the module twin's desired properties
+            // Read the Humidity value from the module twin's desired properties
             var moduleTwin = await ioTHubModuleClient.GetTwinAsync();
             var moduleTwinCollection = moduleTwin.Properties.Desired;
             try
             {
-                temperatureThreshold = moduleTwinCollection["TemperatureThreshold"];
+                humidityFilter = moduleTwinCollection["Humidity"];
             }
             catch (ArgumentOutOfRangeException e)
             {
-                Console.WriteLine($"Property TemperatureThreshold not exist: {e.Message}");
+                Console.WriteLine($"Property Humidity does not exist: {e.Message}");
             }
 
-            // Attach a callback for updates to the module twin's desired properties
+            // Attach a callback for updates to the module twin's properties.
             await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertiesUpdate, null);
 
-            // Register a callback for messages that are received by the module
-            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", FilterMessages, ioTHubModuleClient);
+            // Register callback to be called when a message is received by the module
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", FilterHumidityMessage, ioTHubModuleClient);
         }
 
         /// <summary>
         /// This method is called whenever the module is sent a message from the EdgeHub to inform 
-        /// us that the desired properties have been changed. If a property 'TemperatureThreshold'
-        ///  is found, we take its value and assign it to our temperatureThreshold variable.
+        /// us that the desired properties have been changed. If a property 'Humidity'
+        ///  is found, we take its value and assign it to our humidityFilter variable.
         /// </summary>
         static Task OnDesiredPropertiesUpdate(TwinCollection desiredProperties, object userContext)
         {
@@ -104,9 +104,9 @@ namespace CSharpFilterModule
                 Console.WriteLine("Desired property change: ");
                 Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
 
-                if (desiredProperties["TemperatureThreshold"] != null)
+                if (desiredProperties["Humidity"] != null)
                 {
-                    temperatureThreshold = desiredProperties["TemperatureThreshold"];
+                    humidityFilter = desiredProperties["Humidity"];
                 }
             }
             catch (AggregateException ex)
@@ -128,10 +128,10 @@ namespace CSharpFilterModule
 
         /// <summary>
         /// This method is called whenever the module is sent a message from the EdgeHub. 
-        /// It filters temperatures below the temperature threshold to limit the amount of messages
-        /// that are send to the IoT Hub. It also adds an Alert MessageType property.
+        /// It just pipe the messages without any change.
+        /// It prints all the incoming messages.
         /// </summary>
-        static async Task<MessageResponse> FilterMessages(Message message, object userContext)
+        static async Task<MessageResponse> FilterHumidityMessage(Message message, object userContext)
         {
             var counterValue = Interlocked.Increment(ref counter);
             var result = MessageResponse.Completed;
@@ -150,26 +150,19 @@ namespace CSharpFilterModule
                 // Get the message body
                 var messageBody = JsonConvert.DeserializeObject<MessageBody>(messageString);
 
-                if (messageBody != null && messageBody.machine.temperature > temperatureThreshold)
+                if (messageBody != null && messageBody.ambient.humidity != humidityFilter)
                 {
-                    Console.WriteLine($"Machine temperature {messageBody.machine.temperature} exceeds threshold {temperatureThreshold}");
+                    Console.WriteLine($"Sending message {counterValue} on to the IoTHub!");
                     var filteredMessage = new Message(messageBytes);
                     foreach (KeyValuePair<string, string> prop in message.Properties )
                     {
                         filteredMessage.Properties.Add(prop.Key, prop.Value);
                     }
-                    filteredMessage.Properties.Add("MessageType", "Alert");
                     await moduleClient.SendEventAsync("output1", filteredMessage);
                 }
                 else
                 {
-                    Console.WriteLine($"Sending message {counterValue} on to the next Module!");
-                    var filteredMessage = new Message(messageBytes);
-                    foreach (KeyValuePair<string, string> prop in message.Properties )
-                    {
-                        filteredMessage.Properties.Add(prop.Key, prop.Value);
-                    }
-                    await moduleClient.SendEventAsync("output2", filteredMessage);
+                    Console.WriteLine($"Filter out messages with humidity: {humidityFilter}");
                 }
 
                 // Indicate that the message treatment is completed
