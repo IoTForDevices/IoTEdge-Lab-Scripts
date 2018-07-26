9a99f823e3c7967942004ae97fa3b19b65ea4f18
mstruys <mstruys@hotmail.com>
2018-07-26 09:44:55 +0000

    Added missing Edge Device connection string to create-IoTEdgeVM.sh

--- IoTEdgeDevice/create-IoTEdgeVM.sh
+++ IoTEdgeDevice/create-IoTEdgeVM.sh
@@ -2,6 +2,8 @@
 # This script creates a Linux powered target VM for an IoT Edge Device
 # It also creates an IoTHub and an Azure Container Registry
 
+set -x
+
 now=$(date +"%Y%m%d")
 
 # Set Environment Variables (default names) for the Resource Group,
@@ -49,12 +51,14 @@ NIC_ID_DEV=$(az vm show -g $AZ_RG -n $AZ_EDGE_DEV_VM --query 'networkProfile.net
 SUBNET_ID=$(az network nic show --ids $NIC_ID_DEV -g $AZ_RG --query 'ipConfigurations[].subnet.id' -o tsv)
 
 # Create a VM with an Ubuntu image
+echo "Creating a new Virtual Machine: $AZ_EDGE_VM connected to subnet: $SUBNET_ID"
 az vm create -g $AZ_RG -n $AZ_EDGE_VM --image Canonical:UbuntuServer:16.04-LTS:latest --subnet $SUBNET_ID --generate-ssh-keys --size Standard_B1ms --no-wait
 
 # Create an IoTHub (if you don't want to use an existing IoTHub)
 IOTHUB_FOUND=$(az iot hub list --query "[?name=='$AZ_IOTHUB'].name" -o tsv)
 if [ ! $IOTHUB_FOUND ]
 then
+	echo "Creating a new IoT Hub: $AZ_IOTHUB"
         az iot hub create -g $AZ_RG -n $AZ_IOTHUB --sku S1
 fi
 
@@ -62,17 +66,21 @@ fi
 IOTEDGE_DEV_FOUND=$(az iot hub device-identity list -n $AZ_IOTHUB --query "[?deviceId=='$AZ_EDGE_ID'].capabilities.iotEdge" -o tsv)
 if [ ! $IOTEDGE_DEV_FOUND ]
 then
+	echo "Creating a new IoT Edge Device Identity: $AZ_EDGE_ID on IoT Hub: $AZ_IOTHUB"
 	az iot hub device-identity create -n $AZ_IOTHUB -d $AZ_EDGE_ID -ee
 fi
+IOTEDGE_DEVICE_CS=$(az iot hub device-identity show-connection-string -d $AZ_EDGE_ID -n $AZ_IOTHUB -o tsv)
 
 # Create an Azure Container Registry if one does not yet exist
 ACR_FOUND=$(az acr list --query "[?name=='$AZ_ACR_NAME'].name" -o tsv)
 if [ ! $ACR_FOUND ]
 then
+	echo "Creating a new ACR: $AZ_ACR"
 	az acr create -g $AZ_RG --name $AZ_ACR --ksu Basic
 fi
 
 # Wait for the IoT Edge target VM to be created
+echo "Waiting until the Virtual Machine $AZ_EDGE_VM is created"
 az vm wait -g $AZ_RG -n $AZ_EDGE_VM --created
 
 # Get VM information with queries and set environment variables with (virtual) network info
